72e3b7dfb73cfa38a92c742115f61b4fc5860f02
Shalu-choudhary <choudharyshalu598@gmail.com>
2024-07-17 23:38:58 +0530

    first

--- Greatestelement.java
+++ Greatestelement.java
@@ -1,28 +1,22 @@
-class Greatestelement {
-    int[][] dp = new int[100005][3];
-    int n;
-     int rec(int ind,int sum,int[] a)
-     {
-         if(ind>=n)
-         {
-if(sum!=0)
-return Integer.MIN_VALUE;
-return 0;
-}
-if(dp[ind][sum]!=-1)
-return dp[ind][sum];
-int k=(sum+a[ind])%3;
-int ans1=a[ind]+rec(ind+1,k,a);
-int ans2=rec(ind+1,sum,a);
-return dp[ind][sum]=Math.max(ans1,ans2);
-}
 
-public int maxSumDivThree(int[] nums) { 
-n=nums.length;
-int i,j;
-for(i=0;i<=n;i++)
-for(j=0;j<=2;j++)
-dp[i][j]=-1;
-return rec(0,0,nums);
-}
+public class Greatestelement {
+    public static void main(String[] args) {
+        int[] arr = {3, 6, 5, 1, 8};
+        int maxSum = maxSumDivThree(arr);
+        System.out.println("The greatest sum divisible by three is: " + maxSum);
+    }
+
+    public static int maxSumDivThree(int[] nums) {
+        int[] dp = new int[3]; // dp[i] means the maximum sum which % 3 == i
+        
+        for (int num : nums) {
+            int[] dpNext = dp.clone();
+            for (int i = 0; i < 3; i++) {
+                dpNext[(i + num) % 3] = Math.max(dpNext[(i + num) % 3], dp[i] + num);
+            }
+            dp = dpNext;
+        }
+        
+        return dp[0];
+    }
 }
